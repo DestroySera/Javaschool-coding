a1619f409eefae2cc0e39d31e113c11ad1a88961
HanSeokSeo <56254170+DestroySera@users.noreply.github.com>
2021-05-15 08:31:44 +0900

    Delete prj/src/week01day4 directory

--- prj/src/week01day4/Ex08.java
+++
@@ -1,9 +0,0 @@
-package week01day4;
-
-public class Ex08 {
-	public static void main(String[] args) {
-		int answer = 0;
-		answer = (int)(Math.random()*100)+ 1;
-		System.out.println(answer);
-	}
-}
